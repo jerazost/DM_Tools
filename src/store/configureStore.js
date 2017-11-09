7408a4096943da3ef1b9a9b6a94c2bb6fefdf8cf
Juan Erazo <fersot100@yahoo.com>
2017-11-09 12:31:20 -0800

    Separating monster create form components to work independently

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import monstersReducer from '../reducers/monsterList';
+import monsterCreateReducer from '../reducers/monsterCreate';
 
 export default () => {
   const store = createStore(
     combineReducers({
-    	monsters: monstersReducer
+    	monsters: monstersReducer,
+    	monsterCreate: monsterCreateReducer
     }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
