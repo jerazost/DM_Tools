31d6e9a0f1a09646448802221b242b5269c2bcec
Juan Erazo <fersot100@yahoo.com>
2017-11-26 12:52:28 -0800

    organizing css

--- src/components/create/form/classes.js
+++ src/components/create/form/classes.js
@@ -13,10 +13,41 @@ const CheckboxCard = props => (
 	</div>
 )
 
+const LevelTable = props => {
+	const tableHeads = Object.keys(props);
+	console.log(props[tableHeads[0]]);
+	console.log(props);
+	return (
+		<div>
+			<table className="levelTable">
+				<tbody>
+				<tr>
+					{tableHeads.map((t, i) => 
+						<th key={i}>{t}</th>
+					)}
+				</tr>
+				{
+					props[tableHeads[0]].map((e, i) => 
+						<tr key={i}>
+							{tableHeads.map((t, x) => 
+								<td key={x}>
+									{props[t][i]}
+								</td>
+							)}
+						</tr>
+					)
+				}
+				</tbody>
+			</table>
+		</div>
+		)
+}
+
 const ClassCardPreview = props => (
-	<div className="classCard" data-class={props.name}>
-		<h2>{props.name}</h2>
+	<div className="classCard" data-class={props.name} onClick={props.handleChangeActiveClass}>
+		<h2 data-class={props.name}>{props.name}</h2>
 		<img
+		data-class={props.name}
 		src={`./media/${props.name}.svg`}
 		className="classCard__icon"
 		alt="Class Icon"
@@ -26,7 +57,6 @@ const ClassCardPreview = props => (
 
 
 class ChooseClass extends React.Component{
-
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -34,7 +64,10 @@ class ChooseClass extends React.Component{
 		}
 		this.classList = Object.keys(classes);
 	};
-
+	handleChangeActiveClass = (e) => {
+		const newClass = e.target.dataset.class;
+		if (newClass) this.setState({activeClass: newClass});
+	}
 	render() {
 		return (
 		<div>
@@ -45,11 +78,13 @@ class ChooseClass extends React.Component{
 				</div>
 				<div className="classes__col">
 				{this.classList.map((n, i) => 
-					<ClassCardPreview {...classes[n]} key={i}/>
+					<ClassCardPreview {...classes[n]} 
+					handleChangeActiveClass={this.handleChangeActiveClass} 
+					key={i}/>
 				)}
 				</div>
 				<div className="classes__col">
-					
+					<LevelTable {...classes[this.state.activeClass]["Level Table"]}/>
 				</div>
 				<div className="classes__col">
 					
